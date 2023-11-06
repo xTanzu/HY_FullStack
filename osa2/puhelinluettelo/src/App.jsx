a095899a730c4da3d3d66eb2fb18282afa6ad347
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-11-07 01:36:31 +0200

    2.6: puhelinluettelo step1
    - Make the initial structure of the app.
      - Make it possible to add new names to the app
      - display these names in the bottom of the screen
      - these names dont persist page reload

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+const NumberListing = ({ persons }) => {
+  return (
+    <>
+      <h2>Numbers:</h2>
+      <div className="persons">
+        {persons.map(person => <Person key={person.name} person={person}/>)}
+      </div>
+    </>
+  )
+}
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas" }
+  ]) 
+  const [newName, setNewName] = useState("")
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+    console.log("New Person added!")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addNewPerson}>add</button>
+        </div>
+      </form>
+    <NumberListing persons={persons}/>
+    </div>
+  )
+
+}
+
+export default App
