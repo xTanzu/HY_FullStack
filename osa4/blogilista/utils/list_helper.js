676d65c1450734829186c64bbebc3fc4f172c217
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-12-05 23:56:28 +0200

    4.4: apufunktioita ja yksikkötestejä, step2
    - make function totalLikes to calculate total likes of a list of blogs
    - make tests for this function

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,5 +1,17 @@
 const dummy = blogs => 1
 
+const totalLikes = blogs => {
+
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  const addLikes = (accumulator, current) => accumulator + current.likes
+
+  return blogs.reduce(addLikes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
