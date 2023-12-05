195e252390c36699265f7aa47faa8940cc986cbf
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-12-06 00:59:19 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3
    - define function favoriteBlog() that picks the blog out of a list that
      has most likes and returns its title, author and likes in an object
    - make a test for this function

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,17 +1,37 @@
 const dummy = blogs => 1
 
 const totalLikes = blogs => {
-
   if (blogs.length === 0) {
     return 0
   }
 
   const addLikes = (accumulator, current) => accumulator + current.likes
-
   return blogs.reduce(addLikes, 0)
+}
+
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const reduceToFavourite = (highestIndx, current, indx, blogArray) => {
+    if (current.likes > blogArray[highestIndx].likes) {
+      return indx
+    }
+    return highestIndx
+  }
+
+  favBlogIndx = blogs.reduce(reduceToFavourite, 0)
+  favBlog = blogs[favBlogIndx]
+  return { 
+    title: favBlog.title,
+    author: favBlog.author, 
+    likes: favBlog.likes 
+  }
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
