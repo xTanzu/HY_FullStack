ea68a126ea2c93e64c636753d16fa0b6e472f6d9
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-12-05 21:55:34 +0200

    4.2 blogilista, step2
    - divide the project into smaller modules after the file structure given

---
+++ osa4/blogilista/utils/config.js
@@ -0,0 +1,10 @@
+const conf = require("dotenv").config()
+require("dotenv-expand").expand(conf)
+
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT
+
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
