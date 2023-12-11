e7c5a929a6fdccb7426a34e2d10adc26ef72e84b
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-12-11 23:38:22 +0200

    4.9: blogilistan testit, step2
    - make a test that checks that field called "id" is defined in every
      blog
      - make the field "id" exist by changing toJSON methon in mongoose

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -20,4 +20,12 @@ const blogSchema = mongoose.Schema({
   }
 })
 
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    // delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Blog', blogSchema)
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -14,23 +14,34 @@ beforeEach(async () => {
   await Promise.all(blogPromises)
 })
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/)
-})
+describe("test /api/blogs endpoint", () => {
 
-test("correct amount of blogs are returned", async () => {
-  const response = await api.get("/api/blogs")
-  expect(response.body).toHaveLength(testBlogs.length)
-})
-
-test("correct blogs are returned", async () => {
-  const response = await api.get("/api/blogs")
-  testBlogs.forEach(blog => {
-    expect(response.body).toContainEqual(blog)
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
   })
+
+  test("correct amount of blogs are returned", async () => {
+    const response = await api.get("/api/blogs")
+    expect(response.body).toHaveLength(testBlogs.length)
+  })
+
+  test("correct blogs are returned", async () => {
+    const response = await api.get("/api/blogs")
+    testBlogs.forEach(blog => {
+      expect(response.body).toContainEqual(blog)
+    })
+  })
+
+  test("blogs have field id", async () => {
+    const response = await api.get("/api/blogs")
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
+  })
+
 })
 
 afterAll(async () => {
