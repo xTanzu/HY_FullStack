5d4b1981bce02cc77296d679a0a3519faa7520bd
Taneli Härkönen <taneliharkonen@hotmail.com>
2024-03-21 04:02:56 +0200

    5.7 blogilistan frontend, step7
    - Make blogs toggle between hide and show by clicking a button

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+
+const Blog = ({ blog }) => {
+
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleExpand = event => {
+    setIsExpanded(!isExpanded)
+  }
+
+  const wrapperStyle = {
+    marginBottom: 8,
+    border: "2px solid black",
+  }
+
+  const paragraphStyle = {
+    margin: 4,
+  }
+  console.log(blog)
+
+  return (
+    <div className="blogItemWrapper" style={wrapperStyle}>
+      <p style={paragraphStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleExpand}>{isExpanded ? "hide" : "show"}</button>
+      </p>
+      {isExpanded && <p style={paragraphStyle}>{blog.url}</p>}
+      {isExpanded && <p style={paragraphStyle}>likes: {blog.likes} <button>like</button></p>}
+      {isExpanded && <p style={paragraphStyle}>{blog.user.name}</p>}
+    </div>  
+  )
+}
 
 export default Blog
