48fd6668237933a724469c9a0ef5730b739d2957
Taneli Härkönen <taneliharkonen@hotmail.com>
2024-04-02 12:12:03 +0300

    Add a test
    - Add a test to check that the form does not get emptied if the handling
      of the new blog was not succesful

--- osa5/bloglist-frontend/src/components/BlogForm.test.jsx
+++ osa5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -19,7 +19,7 @@ describe("<BlogForm />", () => {
     user = userEvent.setup()
   })
 
-  test("form calls the callback given to it with correct content", async () => {
+  test("form calls the callback given to it with correct content, and empties the fields", async () => {
     mockAddNewBlogHandler.mockReturnValue({ success: true })
 
     const titleInput = screen.getByTestId("newBlogTitle")
@@ -34,10 +34,29 @@ describe("<BlogForm />", () => {
     expect(mockAddNewBlogHandler).toHaveBeenCalledTimes(1)
     expect(mockAddNewBlogHandler.mock.calls[0][0]).toEqual(inputData)
 
-    // Fields have been emptied
     expect(titleInput.value).toHaveLength(0)
     expect(authorInput.value).toHaveLength(0)
     expect(urlInput.value).toHaveLength(0)
+  })
+
+  test("form does not empty the fields if the handling is not succesful", async () => {
+    mockAddNewBlogHandler.mockReturnValue({ success: false })
+
+    const titleInput = screen.getByTestId("newBlogTitle")
+    const authorInput = screen.getByTestId("newBlogAuthor")
+    const urlInput = screen.getByTestId("newBlogURL")
+    const submitBtn = screen.getByTestId("submitNewBlog")
+    await user.type(titleInput, inputData.title)
+    await user.type(authorInput, inputData.author)
+    await user.type(urlInput, inputData.url)
+    await user.click(submitBtn)
+    
+    expect(mockAddNewBlogHandler).toHaveBeenCalledTimes(1)
+    expect(mockAddNewBlogHandler.mock.calls[0][0]).toEqual(inputData)
+
+    expect(titleInput.value).toHaveLength(inputData.title.length)
+    expect(authorInput.value).toHaveLength(inputData.author.length)
+    expect(urlInput.value).toHaveLength(inputData.url.length)
   })
 
 })
