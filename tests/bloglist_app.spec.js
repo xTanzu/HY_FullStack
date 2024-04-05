b68c5d55200702f325184d3a9da22b3dd14b8f4e
Taneli Härkönen <taneliharkonen@hotmail.com>
2024-04-05 06:13:02 +0300

    5.18: blogilistan end to end ‑testit, step2
    - Make tests for login
      - one test for checking that login is possible with correct
        credentials
      - and one test to check that login is not possible with wrong
        credentials

--- tests/bloglist_app.spec.js
+++ tests/bloglist_app.spec.js
@@ -22,4 +22,36 @@ describe("Bloglist app", () => {
     await expect(page.getByRole("button", { name: "login" })).toBeVisible()
   })
 
+  test("login with correct credentials succeeds", async ({ page }) => {
+    const correctUsername = "mattimeika"
+    const correctPassword = "salainensana1"
+
+    // Vaihtoehto 1
+    // await page.getByRole("textbox").first().fill(correctUsername)
+    // await page.getByRole("textbox").last().fill(correctPassword)
+
+    // Vaihtoehto 2
+    // const textboxes = await page.getByRole("textbox").all()
+    // await textboxes[0].fill(correctUsername)
+    // await textboxes[1].fill(correctPassword)
+
+    // Vaihtoehto 3
+    await page.getByTestId("username_field").fill(correctUsername)
+    await page.getByTestId("password_field").fill(correctPassword)
+
+    await page.getByRole("button", { name: "login" }).click()
+    await expect(page.getByText("Matti Meikäläinen logged in")).toBeVisible()
+  })
+
+  test("login with incorrect credentials does not succeed", async ({ page }) => {
+    const wrongUsername = "hakkinenmika"
+    const wrongPassword = "vääräsalasana0"
+
+    await page.getByTestId("username_field").fill(wrongUsername)
+    await page.getByTestId("password_field").fill(wrongPassword)
+
+    await page.getByRole("button", { name: "login" }).click()
+    await expect(page.getByText("username or password incorrect")).toBeVisible()
+  })
+
 })
