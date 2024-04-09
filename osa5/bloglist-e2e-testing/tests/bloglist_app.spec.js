c0485dc7716d61650cd52113658cc1dacf3077e4
Taneli Härkönen <taneliharkonen@hotmail.com>
2024-04-09 03:44:41 +0300

    Refactor tests to use reusable helper function for login

--- osa5/bloglist-e2e-testing/tests/bloglist_app.spec.js
+++ osa5/bloglist-e2e-testing/tests/bloglist_app.spec.js
@@ -1,16 +1,37 @@
 const { describe, test, expect, beforeEach } = require("@playwright/test")
 
+const resetBackendAndInsertTestData = async (request) => {
+  await request.post("/api/testing/reset")
+  await request.post("/api/users", {
+    data: {
+      username: "mattimeika",
+      name: "Matti Meikäläinen",
+      password: "salainensana1"
+    }
+  })
+}
+
+const loginWith = async (page, username, password) => {
+    // Vaihtoehto 1
+    // await page.getByRole("textbox").first().fill(correctUsername)
+    // await page.getByRole("textbox").last().fill(correctPassword)
+
+    // Vaihtoehto 2
+    // const textboxes = await page.getByRole("textbox").all()
+    // await textboxes[0].fill(correctUsername)
+    // await textboxes[1].fill(correctPassword)
+
+    // Vaihtoehto 3
+    await page.getByTestId("username_field").fill(username)
+    await page.getByTestId("password_field").fill(password)
+
+    await page.getByRole("button", { name: "login" }).click()
+}
+
 describe("Bloglist app", () => {
 
   beforeEach(async ({ page, request }) => {
-    await request.post("http://localhost:3003/api/testing/reset")
-    await request.post("http://localhost:3003/api/users", {
-      data: {
-        username: "mattimeika",
-        name: "Matti Meikäläinen",
-        password: "salainensana1"
-      }
-    })
+    resetBackendAndInsertTestData(request)
     await page.goto("http://localhost:5173")
   })
 
@@ -25,32 +46,14 @@ describe("Bloglist app", () => {
   test("login with correct credentials succeeds", async ({ page }) => {
     const correctUsername = "mattimeika"
     const correctPassword = "salainensana1"
-
-    // Vaihtoehto 1
-    // await page.getByRole("textbox").first().fill(correctUsername)
-    // await page.getByRole("textbox").last().fill(correctPassword)
-
-    // Vaihtoehto 2
-    // const textboxes = await page.getByRole("textbox").all()
-    // await textboxes[0].fill(correctUsername)
-    // await textboxes[1].fill(correctPassword)
-
-    // Vaihtoehto 3
-    await page.getByTestId("username_field").fill(correctUsername)
-    await page.getByTestId("password_field").fill(correctPassword)
-
-    await page.getByRole("button", { name: "login" }).click()
+    loginWith(page, correctUsername, correctPassword)
     await expect(page.getByText("Matti Meikäläinen logged in")).toBeVisible()
   })
 
   test("login with incorrect credentials does not succeed", async ({ page }) => {
     const wrongUsername = "hakkinenmika"
     const wrongPassword = "vääräsalasana0"
-
-    await page.getByTestId("username_field").fill(wrongUsername)
-    await page.getByTestId("password_field").fill(wrongPassword)
-
-    await page.getByRole("button", { name: "login" }).click()
+    loginWith(page, wrongUsername, wrongPassword)
     await expect(page.getByText("username or password incorrect")).toBeVisible()
   })
 
