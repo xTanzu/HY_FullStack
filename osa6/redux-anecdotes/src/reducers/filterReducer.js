012fa05776d6f2786b849e5badffa7b672ad6b0f
Taneli Härkönen <taneliharkonen@hotmail.com>
2024-04-26 01:47:31 +0300

    6.10 anekdootit, step8
    - Install and take into use Redux Toolkit
    - Move redux store initialization to separate file store.js
      - use configureStore to initialize the store
    - Refactor filterReducer to use Redux Toolkits createSlice

--- osa6/redux-anecdotes/src/reducers/filterReducer.js
+++ osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,50 @@
+import  { createSlice } from "@reduxjs/toolkit"
 
-const filterReducer = (state="", action) => {
-  switch(action.type) {
-    case "SET_FILTER":
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    actionSetFilter(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const actionSetFilter = (filter) => {
-  return {
-    type: "SET_FILTER",
-    payload: filter
-  }
-}
+export const { actionSetFilter } = filterSlice.actions
+export default filterSlice.reducer
 
-export default filterReducer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// HERE JUST FOR REFERENCE
+
+// const filterReducer = (state="", action) => {
+//   switch(action.type) {
+//     case "SET_FILTER":
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
+
+// export const actionSetFilter = (filter) => {
+//   return {
+//     type: "SET_FILTER",
+//     payload: filter
+//   }
+// }
+
+// export default filterReducer
