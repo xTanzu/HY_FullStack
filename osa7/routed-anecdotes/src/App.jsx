acbcac036f0974eb07e45036ac491949523745ed
Taneli Härkönen <taneliharkonen@hotmail.com>
2024-09-11 05:33:36 +0300

    7.1: routed anecdotes, step1
    - Use react router to route menu items to url's that route to views
      - there are three views
        - "/" home that lists the anecdotes
        - "/createnew" that presents a form that can be used to add a new
          anecdote
        - "/about" that gives a lorem ipsum about page

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,9 @@
 import { useState } from 'react'
+import { 
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
 
 const Menu = () => {
   const padding = {
@@ -6,9 +11,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to={"/"}>anecdotes</Link>
+      <Link style={padding} to={"/createnew"}>create new</Link>
+      <Link style={padding} to={"/about"}>about</Link>
     </div>
   )
 }
@@ -126,9 +131,11 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/createnew" element={<CreateNew addNew={addNew} />} />
+      </Routes>
       <Footer />
     </div>
   )
