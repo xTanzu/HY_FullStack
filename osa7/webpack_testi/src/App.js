472ea1486f0cb8a155baab9e17f2d634aff25deb
Taneli Härkönen <taneliharkonen@hotmail.com>
2024-12-09 16:25:08 +0200

    Make the database url repend on webpack mode
      - mode "production" uses the remote server
      - any other mode ie. "development" uses local JSON-server

--- osa7/webpack_testi/src/App.js
+++ osa7/webpack_testi/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
 
   const handleClick = () => {
     setCounter(counter + 1)
@@ -15,6 +27,7 @@ const App = () => {
       <button onClick={handleClick}>
         press to count
       </button>
+      <div>{notes.length} notes on the server {BACKEND_URL}</div>
     </div>
   )
 }
--- osa7/webpack_testi/webpack.config.js
+++ osa7/webpack_testi/webpack.config.js
@@ -1,6 +1,13 @@
 const path = require("path")
+const webpack = require("webpack")
 
-const config = () => {
+const config = (env, argv) => {
+  console.log("argv.mode:", argv.mode)
+
+  const backend_url = argv.mode === "production"
+    ? "https://notes2023.fly.dev/api/notes"
+    : "http://localhost:3001/notes"
+
   return {
     entry: "./src/index.js",
     output: {
@@ -28,6 +35,11 @@ const config = () => {
         },
       ],
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
   }
 }
 
