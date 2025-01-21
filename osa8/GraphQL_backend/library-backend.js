4ffa131ba1f79c5a8996fd1740876b183e4bc3d8
Taneli Härkönen <taneliharkonen@hotmail.com>
2025-01-21 03:52:44 +0200

    8.3: kaikki kirjailijat
    - Implement a query allAuthors, where list of authors will be returned
      - type Author will have a field bookCount, where all the books from
        this author will be counted

--- osa8/GraphQL_backend/library-backend.js
+++ osa8/GraphQL_backend/library-backend.js
@@ -106,10 +106,18 @@ const typeDefs = `
     genres: [String]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book]
+    allBooks: [Book]!
+    allAuthors: [Author]!
   }
 `
 
@@ -117,7 +125,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter(book => book.author == root.name).length
   }
 }
 
