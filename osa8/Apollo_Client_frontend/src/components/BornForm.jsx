ce048c988b2b700d088b794b0212043af6624801
Taneli Härkönen <taneliharkonen@hotmail.com>
2025-02-18 04:32:47 +0200

    8.11: Kirjailijan syntymävuosi
    - Add the component BornForm to change an authors born date
      console.log(args)
      - use Apollo Client to send the GraphQL mutatation to the backend
      - make sure that the local state is also updated then the server is
        notified

---
+++ osa8/Apollo_Client_frontend/src/components/BornForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+import { useMutation } from '@apollo/client'
+
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const BornForm = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [ setBornTo ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ],
+    onError: (error) => {
+      const message = error.graphQLErrors.map(e => e.message).join("\n")
+      console.log(message)
+    }
+  })
+
+  const submitBorn = async (event) => {
+    event.preventDefault()
+    const response = await setBornTo({ variables: { name, born: Number(born) } })
+    if (response.data.editAuthor === null) {
+      console.log("author not found")
+      return
+    }
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>set birthyear</h2>
+      <form onSubmit={submitBorn}>
+      <div>
+        <label htmlFor="name">name</label>
+        <br />
+        <input 
+          type="text"
+          id="name"
+          value={name} 
+          onChange={({ target }) => setName(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="born">born</label>
+        <br />
+        <input
+          type="number"
+          id="born"
+          value={born}
+          onChange={({ target }) => setBorn(target.value)}
+        />
+      </div>
+      <button type="submit">set born</button>
+      </form>      
+    </div>
+  )
+}
+
+export default BornForm
--- osa8/Apollo_Client_frontend/src/queries.js
+++ osa8/Apollo_Client_frontend/src/queries.js
@@ -40,3 +40,18 @@ export const ADD_BOOK = gql`
     }
   }
 `
+
+export const EDIT_AUTHOR = gql`
+  mutation EditAuthor(
+    $name: String!,
+    $born: Int!
+  ) {
+    editAuthor(
+      name: $name,
+      setBornTo: $born
+    ) {
+      name
+      born
+    }
+  }
+`
