1e056cd5e6e81ffc51f1e9cc5e4e2ce56284487b
Taneli Härkönen <taneliharkonen@hotmail.com>
2025-10-30 02:48:35 +0200

    8.14: Tietokanta, osa 2
    - Change all remaining resolvers to use MongoDb instead of local data
    - Remove all redundant commented out code and console.logs
    - part 2 ready

--- osa8/GraphQL_backend/library-backend.js
+++ osa8/GraphQL_backend/library-backend.js
@@ -19,102 +19,6 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => console.log("error in connection to MongoDB:", err.message))
 
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = `
   type Book {
     title: String!
@@ -152,13 +56,9 @@ const typeDefs = `
   }
 `
 
-// KAIKKI POIS KOMMENTOITU KOODI ON VANHA PAIKALLINEN TAPA HAKEA DATAA. UUSI VERSIO HAKEE MONGODB:STÄ
-// POISTETAAN KUN KAIKKI VALMIIT
 const resolvers = {
   Query: {
-    // bookCount: () => books.length,
     bookCount: async () => await Book.countDocuments(),
-    // authorCount: () => authors.length,
     authorCount: async () => await Author.countDocuments(),
     allBooks: async (root, args) => { 
       // pipeline pohja joka palauttaa kaikki kirjat populoituna
@@ -188,34 +88,9 @@ const resolvers = {
 
       return await Book.aggregate(pipeline)
     },
-    // allAuthors: () => authors
     allAuthors: async () => Author.find({})
   },
   Mutation: {
-    // addBook: (root, args) => {
-    //   const newBook = (({ title, author, published, genres }) => ({ title, author, published, genres }))(args)
-    //   const bookExists = books.some(book => book.title === newBook.title)
-    //   if (bookExists) {
-    //     throw new GraphQLError('Book must be unique', {
-    //       extensions: {
-    //         code: 'BAD_USER_INPUT',
-    //         invalidArgs: args.title
-    //       }
-    //     })
-    //   }
-    //   books = books.concat(newBook)
-    //   const authorExists = authors.some(author => author.name === newBook.author)
-    //   if (!authorExists) {
-    //     const newAuthor = { 
-    //       name: newBook.author,
-    //       id: uuid_v1(),
-    //       born: null
-    //     }
-    //     authors = authors.concat(newAuthor)
-    //     console.log(newAuthor)
-    //   }
-    //   return newBook
-    // },
     addBook: async (root, args) => {
       let author = await Author.findOne({ name: args.author })
       if (!author) {
@@ -247,30 +122,12 @@ const resolvers = {
       return book.populate("author")
     },
     editAuthor: async (root, args) => {
-      // const foundAuthor = authors.find(author => author.name === args.name)
-      // if (!foundAuthor) {
-      //   console.log("author not found")
-      //   return null
-      // }
-      // console.log("author found, updating")
-      // const updatedAuthor = { ...foundAuthor, born: Number(args.setBornTo) }
-      // authors = authors.map(author => author.name !== args.name ? author : updatedAuthor)
-      // return updatedAuthor
-
-      // const foundAuthor = Author.findOne({name: args.name})
-      // if (!foundAuthor) {
-      //   console.log('author not found')
-      //   return null
-      // }
-      // console.log('author found, updating')
-
       try {
         const updatedAuthor = await Author.findOneAndUpdate(
           { name: args.name }, 
           { born: args.setBornTo },
           { new: true }
         )
-        console.log(updatedAuthor)
         return updatedAuthor
       } catch(exception) {
         throw new GraphQLError('Updating author failed', {
@@ -284,7 +141,6 @@ const resolvers = {
     }
   },
   Author: {
-    // bookCount: (root) => books.filter(book => book.author == root.name).length
     bookCount: async (root) => {
       const pipeline = [
         {
